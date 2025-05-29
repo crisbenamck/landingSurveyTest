c746b62a281913fd2306d2a9ad2bbfe9ba2d12c5
Cristopher Benavides <cristopher_benavides_castillo@mckinsey.com>
2025-05-29 14:19:32 +0200

    fix: Update base URL in Vite config for deployment consistency

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/landingSurveyTest/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
